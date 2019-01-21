1b14f8e703eb98266e8e07c03ab490b8245ee685
LucGulden <33136081+LucGulden@users.noreply.github.com>
2019-01-21 15:53:21 +0100

    Connection à la BDD

--- Serveur-Projet/index.js
+++ Serveur-Projet/index.js
@@ -1,16 +1,38 @@
 // Imports
 var express = require('express');
+var mysql = require('mysql');
 
 // Instantiation du serveur
 var serveur = express();
 
+// Définitions de propriété de la BDD
+var connexion = mysql.createConnection({
+    // Propriétés de la BDD
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wswebservice'
+});
+
+// Connexion à la BDD
+connexion.connect(function(error) {
+
+    if (error) throw error;
+    console.log("Connected!");
+});
+
 // Configuration des routes
 serveur.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Bienvenue sur le serveur du sale</h1>');
+    connexion.query("SELECT * FROM users", function(error, rows, fields) {
+    	if(!!error) {
+    		console.log("Problème");
+    	} else {
+    		console.log(rows);
+    	}
+    });
 });
 
 // Lancement du serveur
-serveur.listen(8080, function() {
+serveur.listen(3000, function() {
     console.log('Serveur démarré');
 });
