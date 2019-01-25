417738f4b66cb9c20394ffc4db3e983e31734c9b
dylanLafarge <38322888+dylanLafarge@users.noreply.github.com>
2019-01-25 10:21:36 +0100

    API GET et POST fonctionnel

--- Serveur-Projet/index.js
+++ Serveur-Projet/index.js
@@ -12,6 +12,7 @@ serveur.use(morgan('short'));
 serveur.use(express.static('./public'));
 
 serveur.use(bodyParser.urlencoded({extended: false}));
+serveur.use(bodyParser.json());
 
 // Définitions de propriété de la BDD
 const connexion = mysql.createConnection({
@@ -32,17 +33,17 @@ connexion.connect(function(error) {
 
 // Configuration des routes
 // GET
-serveur.get('/boite-a-idees', function(req, res) {
+serveur.get('/idee/:ideeId', function(req, res) {
 
     // Définition des paramètre de requête
     //const ideeId = req.body.idee_id;
-    const queryString = "SELECT * FROM idee";
+    const queryString = "SELECT * FROM idee WHERE id_idee = ?";
 
     // Execution de la requête et envoie des données
-    connexionn.query({
+    connexion.query({
         sql: queryString,
-        timeout: 40000
-        //values: []
+        timeout: 40000,
+        values: [req.params.ideeId]
         }, function(error, rows, fields) {
             if(error) {
                 console.log("Impossible d'accéder aux idées : " + error);
@@ -54,25 +55,41 @@ serveur.get('/boite-a-idees', function(req, res) {
                 console.log(rows);
             }
         });
+});
 
-    
+serveur.get('/idee', function(req, res) {
+
+    // Définition des paramètre de requête
+    //const ideeId = req.body.idee_id;
+    const queryString = "SELECT * FROM idee";
+
+    // Execution de la requête et envoie des données
+    connexion.query({
+        sql: queryString,
+        timeout: 40000
+        }, function(error, rows, fields) {
+            if(error) {
+                console.log("Impossible d'accéder aux idées : " + error);
+                res.sendStatus(500);
+                throw error;
+            } else {
+                console.log("Succès de la requête GET");
+                res.json(rows);
+                console.log(rows);
+            }
+        });
 });
 
 //POST
-serveur.post('/proposition-idee', (req, res) => {
+serveur.post('/', (req, res) => {
     console.log("Tentative de création d'une nouvelle idée...");
 
-    const titre = req.body.idee_titre;
-    const description = req.body.idee_description;
-    const id_user = req.body.id_user;
-    
+    const queryString = "INSERT INTO idee (titre_idee, description_idee, id_users) VALUES (?, ?, 2)"; 
 
-    const queryString = "INSERT INTO users (username, user_email, user_role, user_status) VALUES (?, ?, ?, ?)"; 
-
-    connexionn.query({
+    connexion.query({
         sql: queryString,
-        timeout: 40000
-        //values: []
+        timeout: 40000,
+        values: [req.body.titre_idee, req.body.description_idee]
         }, function(error, rows, fields) {
             if(error) {
                 console.log("Impossible d'accéder aux idées : " + error);
